8749b6810875897909e21a39b3d38962ba8b6b3a
Puja Shah <pks37@sfu.ca>
2026-01-21 23:52:51 -0800

    feat: trigger vercel redeploy

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,6 +6,7 @@ import gsap from "gsap"
 import { ScrollTrigger } from "gsap/ScrollTrigger"
 import { useRef,useEffect,useState } from "react";
 
+//about
 //registed plugin
 gsap.registerPlugin(ScrollTrigger);
 
