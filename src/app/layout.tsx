dc4633462f94c73afb9634f73fc682791937b1e1
Puja Shah <pks37@sfu.ca>
2026-01-28 19:01:19 -0800

    fix: added name metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,11 @@ import {inter, playfair} from "../fonts"
 import "./globals.css";
 import { GoogleAnalytics } from '@next/third-parties/google';
 
+export const metadata = {
+  title: "Humane The Thrift Store",
+  description: "Ethical. Local. Circular ",
+};
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
